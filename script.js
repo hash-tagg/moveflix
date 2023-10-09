a24b23a712b4cb9ba1e78605007ebc2564c7072b
Akash Yadav <ashu2612yadav@gmail.com>
2023-10-09 09:41:07 +0530

    api key error resolve

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ function loadMovieDetails(){
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=e2e3b5fb`);
+            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=e199cdac`);
             const movieDetails = await result.json();
             // console.log(movieDetails);
             displayMovieDetails(movieDetails);
